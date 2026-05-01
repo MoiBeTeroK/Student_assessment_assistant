08cfbed391c663778c567fcd9efe9324c21fd213
Мария Масенко <mariamasenko@Marias-MacBook-Air.local>
2026-05-01 20:17:21 +0300

    (feat): add settings page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme';
 import { LoginPage } from '../pages/login';
+import { SettingsPage } from '../pages/settings';
 
 export const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <LoginPage />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                    <Route path="*" element={<Navigate to="/login" replace />} />
+                </Routes>
+            </BrowserRouter>
         </ThemeProvider>
     );
 };
